b43103200cfd4c5b174aa2d6f6994eaf3df7e706
kingmusab-tmt <musab.buraimoh@gmail.com>
2023-08-10 14:01:19 -0700

    betty style followed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *   
+ * Return: Always 0 (Success)
+ */
 int main()
 {
 	char magic[] = "\"Programming is like building a multilingual puzzle\n";
+	
 	puts(magic);
-return (0);
+
+	return (0);
 }
