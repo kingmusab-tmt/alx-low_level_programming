c0c390d22034b5c136d914f91db54a2978c873fe
kingmusab-tmt <musab.buraimoh@gmail.com>
2023-08-10 14:34:42 -0700

    4 trail

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-char magic[] = "\"Programming is like building a multilingual puzzle";
-puts(magic);
+	char magic[]="\"Programming is like building a multilingual puzzle";
+	puts(magic);
 return (0);
 }
