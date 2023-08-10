2a14022a156b1bba542b13752bfaee23eeb5f0af
kingmusab-tmt <musab.buraimoh@gmail.com>
2023-08-10 13:48:56 -0700

    betty style included

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-	char magic[] = "Programming is like building a multilingual puzzle\n";
+	char magic[] = "\"Programming is like building a multilingual puzzle\n";
 	puts(magic);
 return (0);
 }
