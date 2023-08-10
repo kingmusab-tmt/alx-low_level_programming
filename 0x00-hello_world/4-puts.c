b15dbe1bfacf04eeb74ff47ae750d4b4a088d16f
kingmusab-tmt <musab.buraimoh@gmail.com>
2023-08-10 14:58:34 -0700

    trailing whitespace solved

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *   
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char magic[] = "\"Programming is like building a multilingual puzzle";
+	char magic[] = "\"Programming is like building a multilingual puzzle";
 
-puts(magic);
+	puts(magic);
 return (0);
 }
