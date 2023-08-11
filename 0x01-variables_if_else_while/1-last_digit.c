b2952a6b4cd05407192c92e9ddb53730afd7fa43
kingmusab-tmt <musab.buraimoh@gmail.com>
2023-08-11 10:43:59 -0700

    space error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n % 10 != 0 && n % 10 < 6)
 	{
-		printf("Last digit of %d is %d and is less than", n, (n % 10));
+		printf("Last digit of %d is %d and is less than ", n, (n % 10));
 		printf("6 and not 0\n");
 	} else if (n % 10 == 0)
 	{
