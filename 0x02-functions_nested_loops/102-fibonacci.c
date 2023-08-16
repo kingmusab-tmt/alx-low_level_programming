55ecf42ef6491d402189f51d71cf9ce6a39acfde
kingmusab-tmt <musab.buraimoh@gmail.com>
2023-08-16 02:21:26 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 return success
+ */
+int main(void)
+{
+	int fib[50];
+	int i;
+
+	fib[0] = 1;
+	fib[1] = 2;
+
+	for (i = 2; i < 50; i++)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+	}
+	for (i = 0; i < 49; i++)
+	{
+		printf("%d, ", fib[i]);
+	}
+	printf("%d\n", fib[49]);
+	return (0);
+}
