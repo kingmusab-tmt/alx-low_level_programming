dcdbd5a4fc7b2beb6dbc1ebf207cb968eddcb9a6
kingmusab-tmt <musab.buraimoh@gmail.com>
2023-08-16 02:32:37 -0700

    103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 return success
+ */
+int main(void)
+{
+	unsigned long fib1 = 1;
+
+	unsigned long fib2 = 2;
+	unsigned long sum = 0;
+
+	while (fib2 <= 4000000)
+	{
+		if (fib2 % 2 == 0)
+		{
+			sum += fib2;
+		}
+		unsigned long temp = fib2;
+
+		fib2 = fib1 + fib2;
+		fib1 = temp;
+	}
+	printf("%llu\n", sum);
+	return (0);
+}
+
