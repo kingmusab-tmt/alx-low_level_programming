ff1a2093c3cf3d2278716c854a58a7307fc3acad
kingmusab-tmt <musab.buraimoh@gmail.com>
2023-08-16 02:01:16 -0700

    100 times trying

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,11 +14,13 @@ void print_times_table(int n)
 	int column;
 
 		if (n < 0 || n > 15)
+		{
 			return;
+		}
 
 	for (row = 0; row <= n; row++)
 	{
-		for (column = 1; column <= n; column++)
+		for (column = 0; column <= n; column++)
 		{
 			int result = row * column;
 
