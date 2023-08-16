9e89b9ebb145505c0785c0cca833e16eb6f931b6
kingmusab-tmt <musab.buraimoh@gmail.com>
2023-08-16 08:38:08 -0700

    first time debugging in c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - test function that prints if integer is posotive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
