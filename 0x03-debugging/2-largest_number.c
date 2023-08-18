1b45dbe5a21e22e3fa482e836925993ef5668013
kingmusab-tmt <musab.buraimoh@gmail.com>
2023-08-17 22:08:43 -0700

    guess soluion

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -19,7 +19,10 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 		return (largest);
-	}
+	} else if (c > a && c > b)
+	{
 		largest = c;
-	return (largest);
+		return (largest);
+	} 
+	return (0);
 }
