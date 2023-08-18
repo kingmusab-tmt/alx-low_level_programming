e44e03a87fcb8b47b74a79b3e5901d77fe7b9695
kingmusab-tmt <musab.buraimoh@gmail.com>
2023-08-17 22:30:46 -0700

    largest bug

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,13 +13,13 @@ int largest_number(int a, int b, int c)
 	if (a > b && a > c)
 	{
 		largest = a;
-		return (largest);
 	}
 	else if (b > a && b > c)
 	{
 		largest = b;
-		return (largest);
-	}
+	} else
+	{
 		largest = c;
-		return (largest);
+	}
+	return (largest);
 }
