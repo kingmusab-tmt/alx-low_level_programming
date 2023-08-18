52bcd62004020b108536b1ffcd8fa390a70a257e
kingmusab-tmt <musab.buraimoh@gmail.com>
2023-08-17 21:25:37 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
 /**
  * main - test function that prints if integer is posotive or negative
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 	return (0);
