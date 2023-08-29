4de8205cd02d5c1f3930c5bd2eb83ae0b412c374
kingmusab-tmt <musab.buraimoh@gmail.com>
2023-08-29 06:58:54 -0700

    modified to pass result

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -24,7 +24,7 @@ char *_strchr(char *s, char c)
 		if (s[i] == '\0')
 		{
 		return (NULL);
-		}	
+		}
 	}
 	return (NULL);
 }
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,14 +14,18 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 	int j;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; haystack[j] != '\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[i] == haystack[j] && needle[i] != '\0')
+			if (needle[j] != haystack[i + j])
 			{
-				return (&needle[i]);
+				break;
 			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (&haystack[i]);
 		}
 	}
 	return (NULL);
