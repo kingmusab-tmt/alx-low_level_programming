0b2fcbb818b24ff1f2e49f6e3f82edc19b8ece7e
kingmusab-tmt <musab.buraimoh@gmail.com>
2023-08-30 18:41:44 -0700

    recursion enforced

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -14,23 +14,32 @@ int _strlen_recursion(char *s)
 	return (0);
 }
 /**
+ * is_palindrome_recursive - Check if a string is a palindrome recursively.
+ * @s: The string to check.
+ * @forward: The index for the forward traversal.
+ * @backward: The index for the backward traversal.
+ * Return: 1 if the string is a palindrome, 0 if not.
+ */
+int is_palindrome_recursive(char *s, int forward, int backward)
+{
+	if (forward >= backward)
+	{
+		return (1);
+	}
+	if (s[forward] != s[backward])
+	{
+		return (0);
+	}
+	return (is_palindrome_recursive(s, forward + 1, backward - 1));
+}
+/**
  * is_palindrome - Check if a string is a palindrome.
  * @s: The string to check.
  * Return: 1 if the string is a palindrome, 0 if not.
  */
 int is_palindrome(char *s)
 {
-	int forward = 0;
-	int backward = _strlen_recursion(s) - 1;
+	int length = _strlen_recursion(s);
 
-	while (forward < backward)
-	{
-		if (s[forward] != s[backward])
-		{
-			return (0);
-		}
-		forward++;
-		backward--;
-	}
-	return (1);
+	return (is_palindrome_recursive(s, 0, length - 1));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,24 @@
 #include "main.h"
 /**
+ * primeValue - function to check the prime
+ * @n: actual number
+ * @i: value for product
+ *
+ * Return: 1 or 0
+ */
+int primeValue(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return primeValue(n, i + 1);
+}
+/**
  * is_prime_number - function that checks for primse number
  *
  * @n: number to be checked
@@ -7,18 +26,9 @@
  */
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n < 0 || n <= 1)
 	{
 		return (0);
 	}
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (primeValue(n, 2));
 }
